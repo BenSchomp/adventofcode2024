78e225b279de4b30159f53d8fcde9f2150022ea2
Ben Schomp <ben@benschomp.com>
2024-12-02 01:11:00 -0500

    2 done

--- day-02.py
+++ day-02.py
@@ -1,37 +1,45 @@
-file = open('day-02.txt', 'r')
-
-
 def isSafe(row):
   direction = None
   prev = row[0]
 
-  #print( row )
   for r in row[1:]:
     delta = r - prev
-    #print( r, prev, delta, direction )
     if delta == 0 or abs(delta) > 3:
-      #print( 'FAIL, delta:', delta )
       return False
 
     if not direction:
       direction = delta / abs(delta)
-    else:
-      if direction != delta / abs(delta):
-        #print( 'FAIL, direction:', direction )
-        return False
+    elif direction != delta / abs(delta):
+      return False
 
     prev = r
 
-  #print( 'safe!' )
   return True
 
-part_one = 0
+def isDampenerSafe(row):
+  for i in range(len(row)):
+    test = row[:i] + row[i+1:]
+    if isSafe(test):
+      return True
+
+  return False
+
+# --- main ---
+
+file = open('day-02.txt', 'r')
+
+part_one = part_two = 0
 for line in file:
   line = [int(x) for x in line.strip().split()]
 
   if isSafe(line):
     part_one += 1
+    part_two += 1
+
+  elif isDampenerSafe(line):
+    part_two += 1
 
 file.close()
 
 print( part_one )
+print( part_two )
