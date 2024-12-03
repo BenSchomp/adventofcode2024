08a9e0f5a43388e68a9c583e1f74b127884c829c
Ben Schomp <ben@benschomp.com>
2024-12-03 05:27:07 -0500

    p2d

--- day-03.py
+++ day-03.py
@@ -1,18 +1,42 @@
 import re
 
-memory = 'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'
-
-part_one = 0
-
-file = open('day-03.txt', 'r')
-for line in file:
-  instructions = re.findall('mul\(\d{1,3},\d{1,3}\)', line)
-
-  print( instructions )
+def decrypt(memory):
+  result = 0
+  instructions = re.findall(r'mul\(\d{1,3},\d{1,3}\)', memory)
 
   for i in instructions:
     (f1, f2) = i.split('(')[1][:-1].split(',')
-    part_one += (int(f1) * int(f2))
+    result += (int(f1) * int(f2))
+
+  return result
+
+def decrypt2(memory):
+  global enabled
+  result = 0
+  instructions = re.findall(r'(mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\))', memory)
+
+  for i in instructions:
+    if i == "do()":
+      enabled = True
+    elif i == "don't()":
+      enabled = False;
+    elif i[:3] == "mul" and enabled:
+      (f1, f2) = i.split('(')[1][:-1].split(',')
+      result += (int(f1) * int(f2))
+
+  return result
+
+# --- main ---
+
+enabled = True
+part_one = part_two = 0
+
+file = open('day-03.txt', 'r')
+for line in file:
+  part_one += decrypt(line)
+  part_two += decrypt2(line)
+
+file.close()
 
 print( part_one )
-
+print( part_two )
