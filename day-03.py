026e13c7efd69a0f03b368d254670219b6898c27
Ben Schomp <ben@benschomp.com>
2024-12-03 14:26:09 -0500

    cleanup

--- day-03.py
+++ day-03.py
@@ -1,26 +1,20 @@
 import re
 
-def decrypt(memory):
-  result = 0
-  instructions = re.findall(r'mul\(\d{1,3},\d{1,3}\)', memory)
-
-  for i in instructions:
-    (f1, f2) = i.split('(')[1][:-1].split(',')
-    result += (int(f1) * int(f2))
-
-  return result
-
-def decrypt2(memory):
+def execute(memory, part):
   global enabled
   result = 0
-  instructions = re.findall(r'(mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\))', memory)
+
+  if part == 1:
+    instructions = re.findall(r'mul\(\d{1,3},\d{1,3}\)', memory)
+  else:
+    instructions = re.findall(r'(mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\))', memory)
 
   for i in instructions:
     if i == "do()":
       enabled = True
     elif i == "don't()":
       enabled = False;
-    elif i[:3] == "mul" and enabled:
+    elif (part == 1 or enabled) and i[:3] == "mul":
       (f1, f2) = i.split('(')[1][:-1].split(',')
       result += (int(f1) * int(f2))
 
@@ -33,8 +27,8 @@ part_one = part_two = 0
 
 file = open('day-03.txt', 'r')
 for line in file:
-  part_one += decrypt(line)
-  part_two += decrypt2(line)
+  part_one += execute(line, 1)
+  part_two += execute(line, 2)
 
 file.close()
 
