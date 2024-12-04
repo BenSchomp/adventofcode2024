8a83982f41b5deb1e936a48ad3fd102c05c2cf8b
Ben Schomp <ben@benschomp.com>
2024-12-04 08:07:12 -0500

    day 4, part 2

--- day-04.py
+++ day-04.py
@@ -10,27 +10,38 @@ def findWord( startY, startX, word ):
       x = startX
       y = startY
       curWord = grid[y][x]
-      #print( x, y, grid[y][x] )
 
       x += dx
       y += dy
       while( x > -1 and y > -1 and x < width and y < height ):
-        #print( x, y, grid[y][x] )
         curWord += grid[y][x]
         if curWord == word:
           count += 1
-          #print( 'match!')
           break
 
         if curWord != word[:len(curWord)]:
-          #print( 'no match')
           break
 
         x += dx
         y += dy
-      #print( '-' )
 
   return count
+
+def findWord2( y, x ):
+  global grid, width, height
+
+  if x == 0 or x == width-1 or y == 0 or y == height-1:
+    return 0
+
+  if not( (grid[y-1][x+1] == 'M' and grid[y+1][x-1] == 'S') or\
+          (grid[y-1][x+1] == 'S' and grid[y+1][x-1] == 'M') ):
+    return 0
+
+  if not( (grid[y+1][x+1] == 'M' and grid[y-1][x-1] == 'S') or\
+          (grid[y+1][x+1] == 'S' and grid[y-1][x-1] == 'M') ):
+    return 0
+
+  return 1
 
 #--- main ---
 grid = []
@@ -46,11 +57,14 @@ file.close()
 width = len(grid[0])
 height = len(grid)
 
-part_one = 0
+part_one = part_two = 0
 for r in range(height):
   for c in range(width):
     if grid[r][c] == 'X':
       part_one += findWord( r, c, 'XMAS' )
-
+    if grid[r][c] == 'A':
+      part_two += findWord2( r, c )
 
 print( part_one )
+print( part_two )
+
