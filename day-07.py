d83c0ca31d817eb83dbb4e2dfa8ef34ce9e4d082
Ben Schomp <ben@benschomp.com>
2024-12-07 00:56:17 -0500

    clean up!

--- day-07.py
+++ day-07.py
@@ -1,55 +1,34 @@
-def solve( r, v ):
-  # this is recursive
-  n = len(v)
-
-  if n == 2:
-    if v[0] + v[1] == r:
-      #print( '  GOOD:', v[0], '+', v[1], '=', r )
-      return True
-    elif v[0] * v[1] == r:
-      #print( '  GOOD:', v[0], '*', v[1], '=', r )
-      return True
-    else:
-      return False
-
-  else:
-    result = solve( r, [v[0]+v[1]] + v[2:] )
-    if result:
-      return True
-    else:
-      return solve( r, [v[0]*v[1]] + v[2:] )
-
+# concatenate 2 ints (returns an int)
 def concat( a, b ):
   return int( str(a) + str(b) )
 
-def solve2( r, v ):
-  # this is recursive
-  n = len(v)
-  
-  if n == 2:
+# this is recursive (if part two, call w p2 as True)
+def solve( r, v, p2=False ):
+  # base case: 2 values, try all the operators and see if we get a match
+  if len(v) == 2:
     if v[0] + v[1] == r:
-      print( '  GOOD:', v[0], '+', v[1], '=', r )
       return True
     elif v[0] * v[1] == r:
-      print( '  GOOD:', v[0], '*', v[1], '=', r )
       return True
-    elif concat(v[0], v[1]) == r:
-      print( '  GOOD:', v[0], '||', v[1], '=', r )
+    elif p2 and concat(v[0], v[1]) == r:
       return True
     else:
       return False
 
+  # else recurse, iterating through the operators on the 1st two values
   else:
-    result = solve2( r, [v[0]+v[1]] + v[2:] )
+    result = solve( r, [v[0]+v[1]] + v[2:], p2 )
     if result:
       return True
 
-    result = solve2( r, [v[0]*v[1]] + v[2:] )
+    result = solve( r, [v[0]*v[1]] + v[2:], p2 )
     if result:
       return True
 
+    if p2:
+      return solve( r, [concat(v[0], v[1])] + v[2:], p2 )
     else:
-      return solve2( r, [concat(v[0], v[1])] + v[2:] )
+      return False
 
 # --- main ---
 part_one = part_two = 0
@@ -62,11 +41,12 @@ for line in file:
   result = int(left)
   values = [int(x) for x in right.split()]
 
-  print( result, values )
   if solve( result, values ):
     part_one += result
-  if solve2( result, values ):
+
+  if solve( result, values, True ):
     part_two += result
+
 file.close()
 
 print( part_one )
