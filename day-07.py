62839dd945a05f6f8c0411b2e08c23391a4114a2
Ben Schomp <ben@benschomp.com>
2024-12-07 00:43:29 -0500

    and p2 under 40 yay

--- day-07.py
+++ day-07.py
@@ -4,10 +4,10 @@ def solve( r, v ):
 
   if n == 2:
     if v[0] + v[1] == r:
-      print( '  GOOD:', v[0], '+', v[1], '=', r )
+      #print( '  GOOD:', v[0], '+', v[1], '=', r )
       return True
     elif v[0] * v[1] == r:
-      print( '  GOOD:', v[0], '*', v[1], '=', r )
+      #print( '  GOOD:', v[0], '*', v[1], '=', r )
       return True
     else:
       return False
@@ -19,8 +19,40 @@ def solve( r, v ):
     else:
       return solve( r, [v[0]*v[1]] + v[2:] )
 
+def concat( a, b ):
+  return int( str(a) + str(b) )
+
+def solve2( r, v ):
+  # this is recursive
+  n = len(v)
+  
+  if n == 2:
+    if v[0] + v[1] == r:
+      print( '  GOOD:', v[0], '+', v[1], '=', r )
+      return True
+    elif v[0] * v[1] == r:
+      print( '  GOOD:', v[0], '*', v[1], '=', r )
+      return True
+    elif concat(v[0], v[1]) == r:
+      print( '  GOOD:', v[0], '||', v[1], '=', r )
+      return True
+    else:
+      return False
+
+  else:
+    result = solve2( r, [v[0]+v[1]] + v[2:] )
+    if result:
+      return True
+
+    result = solve2( r, [v[0]*v[1]] + v[2:] )
+    if result:
+      return True
+
+    else:
+      return solve2( r, [concat(v[0], v[1])] + v[2:] )
+
 # --- main ---
-part_one = 0
+part_one = part_two = 0
 file = open('day-07.txt', 'r')
 
 for line in file:
@@ -33,6 +65,9 @@ for line in file:
   print( result, values )
   if solve( result, values ):
     part_one += result
+  if solve2( result, values ):
+    part_two += result
 file.close()
 
 print( part_one )
+print( part_two )
