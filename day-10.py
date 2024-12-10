8d166588380ef8d6373c08ef07d354cfe965dfb6
Ben Schomp <ben@benschomp.com>
2024-12-10 00:58:06 -0500

    day 10, combined cleanup

--- day-10.py
+++ day-10.py
@@ -3,26 +3,6 @@ def score( curX, curY ):
   global grid, width, height
   elevation = grid[curY][curX]
 
-  for (dx, dy) in [(0,-1),(1,0),(0,1),(-1,0)]:
-    newX = curX + dx
-    newY = curY + dy
-
-    if newX < 0 or newX >= width or newY < 0 or newY >= height:
-      continue
-
-    if grid[newY][newX] != elevation + 1:
-      continue
-
-    if grid[newY][newX] == 9:
-      if (newX, newY) not in summits:
-        summits.add( (newX, newY) )
-    else:
-      score( newX, newY )
-
-def score2( curX, curY ):
-  global grid, width, height
-  elevation = grid[curY][curX]
-
   result = 0
   for (dx, dy) in [(0,-1),(1,0),(0,1),(-1,0)]:
     newX = curX + dx
@@ -36,11 +16,12 @@ def score2( curX, curY ):
 
     if grid[newY][newX] == 9:
       result += 1
+      if (newX, newY) not in summits:
+        summits.add( (newX, newY) )
     else:
-      result += score2( newX, newY )
-    
-  return result
+      result += score( newX, newY )
 
+  return result
 
 # --- main ---
 grid = []
@@ -62,9 +43,8 @@ for row in range(height):
   for col in range(width):
     if grid[row][col] == 0:
       summits = set()
-      score( col, row )
+      part_two += score( col, row )
       part_one += len(summits)
-      part_two += score2( col, row )
 
 print( part_one )
 print( part_two )
