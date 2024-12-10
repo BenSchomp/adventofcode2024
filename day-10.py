cbf90db8c1f2f6795621d6a95c868aa1541a24f4
Ben Schomp <ben@benschomp.com>
2024-12-10 00:55:27 -0500

    part 2 done

--- day-10.py
+++ day-10.py
@@ -19,6 +19,29 @@ def score( curX, curY ):
     else:
       score( newX, newY )
 
+def score2( curX, curY ):
+  global grid, width, height
+  elevation = grid[curY][curX]
+
+  result = 0
+  for (dx, dy) in [(0,-1),(1,0),(0,1),(-1,0)]:
+    newX = curX + dx
+    newY = curY + dy
+
+    if newX < 0 or newX >= width or newY < 0 or newY >= height:
+      continue
+
+    if grid[newY][newX] != elevation + 1:
+      continue
+
+    if grid[newY][newX] == 9:
+      result += 1
+    else:
+      result += score2( newX, newY )
+    
+  return result
+
+
 # --- main ---
 grid = []
 trailheads = []
@@ -34,12 +57,14 @@ file.close
 height = len(grid)
 width = len(grid[0])
 
-part_one = 0
+part_one = part_two = 0
 for row in range(height):
   for col in range(width):
     if grid[row][col] == 0:
       summits = set()
       score( col, row )
       part_one += len(summits)
+      part_two += score2( col, row )
 
 print( part_one )
+print( part_two )
